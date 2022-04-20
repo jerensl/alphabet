60c3ab6291d0e5a5cfb7077990efa2fc5c639a6f
jerensl <54782057+jerensl@users.noreply.github.com>
2022-04-20 18:37:38 +0800

    on costumer-api service change express framework to fastify

--- apps/customer-api/src/main.ts
+++ apps/customer-api/src/main.ts
@@ -1,7 +1,7 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
@@ -9,11 +9,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({
+      logger: true,
+    })
+  );
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
   );
